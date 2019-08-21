ae80f83418bc9422b251437798d155d4d3cd3148
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-08-20 23:20:38 -0500

    feat: add AuthContext

---
+++ src/context/AuthContext.js
@@ -0,0 +1,56 @@
+import React, { useReducer, useContext } from 'react';
+import PropTypes from 'prop-types';
+
+const AuthContext = React.createContext({ user: null });
+
+function authReducer(state, action) {
+  switch (action.type) {
+    case 'SIGN_IN':
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case 'SIGN_OUT':
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      return state;
+  }
+}
+
+const AuthProvider = props => {
+  const initialState = useContext(AuthContext);
+  const [state, dispatch] = useReducer(authReducer, initialState);
+  const { user } = state;
+  const { children } = props;
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        state,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.element]),
+};
+
+AuthProvider.defaultProps = {
+  children: PropTypes.element,
+};
+
+const AuthConsumer = AuthContext.Consumer;
+
+const withAuth = Component => props => (
+  <AuthContext.Consumer>{state => <Component {...props} context={state} />}</AuthContext.Consumer>
+);
+
+export { AuthContext, AuthProvider, AuthConsumer, withAuth };

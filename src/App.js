6d63c644289cd555c3f37f115073df9b9fac230b
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-08-20 23:10:50 -0500

    perf: add Layout and AuthProvider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
+// ▶ Import react dependecies
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MemoryRouter as Router } from 'react-router-dom';
 
-function App() {
+// ▶ Import components
+import Layout from './components/Layout';
+import { AuthProvider } from './context/AuthContext';
+
+const App = () => {
+  // the mui theme is used in the themeProvider.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Layout />
+      </Router>
+    </AuthProvider>
   );
-}
+};
 
 export default App;

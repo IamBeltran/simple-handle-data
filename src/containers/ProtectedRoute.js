3d9131c9bc135243ee02585af899bae5a8deae59
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-09-03 20:57:12 -0500

    refact: change AuthConsumer to AuthContext

--- src/containers/ProtectedRoute.js
+++ src/containers/ProtectedRoute.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
-import { AuthConsumer } from '../context/AuthContext';
+import { AuthContext } from '../context/AuthContext';
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const { user } = useContext(AuthContext);
   return (
-    <AuthConsumer>
-      {({ user }) => (
-        <Route
-          {...rest}
-          render={props =>
-            user ? <Component {...props} /> : <Redirect to={{ pathname: '/signinform' }} />
-          }
-        />
-      )}
-    </AuthConsumer>
+    <React.Fragment key="ProtectedRoute">
+      <Route
+        {...rest}
+        render={props =>
+          user ? <Component {...props} /> : <Redirect to={{ pathname: '/signinform' }} />
+        }
+      />
+    </React.Fragment>
   );
 };
 

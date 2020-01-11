114a1160cdc3b331277dd37c345cb0e12f7603e6
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2020-01-11 00:14:59 -0600

    style: add comments

--- public/electron.js
+++ public/electron.js
@@ -71,6 +71,8 @@ const startUrl = isDev
 //  │ DECLARATION OF AUXILIARY FUNCTIONS.                                               │
 //  └───────────────────────────────────────────────────────────────────────────────────┘
 function createWindow() {
+  // SECTION: mainWindow
+  // » Create the browser window (mainWindow)
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -84,7 +86,7 @@ function createWindow() {
     },
   });
 
-  // Show the mainWindow when it is loaded and ready to show
+  // » And load the index.html of the app.
   mainWindow.loadURL(startUrl);
 
   if (!isDev) {
@@ -104,39 +106,50 @@ function createWindow() {
     });
   }
 
-  // Show the mainWindow when it is loaded and ready to show
+  // » Emitted when the web page has been rendered (while not being shown)
+  // » and window can be displayed without a visual flash.
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
   });
 
-  // Show the mainWindow when it is loaded and ready to show
+  // » Emitted when the window is closed.
   mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
     mainWindow = null;
   });
+  // !SECTION
 }
 
 //  ┌───────────────────────────────────────────────────────────────────────────────────┐
 //  │ APPLICATION'S EVENT LISTENERS                                                     │
 //  └───────────────────────────────────────────────────────────────────────────────────┘
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+// app.on('ready', createWindow);
+// » Emitted when Electron has finished initializing.
 app.on('ready', () => {
   createWindow();
 });
 
+// » Emitted when all windows have been closed.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  // Quit when all windows are closed.
+  if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createWindow();
 });
 
 //  ┌───────────────────────────────────────────────────────────────────────────────────┐
 //  │ IPC'S EVENT LISTENERS (Inter-Process Communication)                               │
 //  └───────────────────────────────────────────────────────────────────────────────────┘
+// » SEND-NOTIFICATION
 ipcMain.on('send-notification', (event, notification) => {
   notifier.notify({
     title: notification.title,
@@ -144,6 +157,7 @@ ipcMain.on('send-notification', (event, notification) => {
   });
 });
 
+// » LOGIN-SEND
 ipcMain.on('login-send', async (event, { email, password }) => {
   await doSignInWithEmailAndPassword(email, password)
     .then(user => {
@@ -154,6 +168,7 @@ ipcMain.on('login-send', async (event, { email, password }) => {
     });
 });
 
+// » LOGOUT-SEND
 ipcMain.on('logout-send', async (event, { email, password }) => {
   await doSignOut(email, password)
     .then(() => {
